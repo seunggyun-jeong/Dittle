c57ec96ad39ae110054ac610c924aa6417efdfe3
seunggyun-jeong <jskyun98@gmail.com>
2024-08-06 10:44:45 +0900

    [Init] Project Init

---
+++ Dittle/Dittle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dittle
+//
+//  Created by Gyunni on 8/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dittle/Dittle/DittleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DittleApp.swift
+//  Dittle
+//
+//  Created by Gyunni on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct DittleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
